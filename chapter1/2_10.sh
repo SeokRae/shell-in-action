79fccadd42cf10fcc78bac8aed59aa849c26fc29
seokrae <kslbsh@gmail.com>
2022-01-20 22:01:50 +0900

    feat: parameters

---
+++ chapter1/2_10.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# sh 2_10.sh Korean English "Japanese Chinese"
+
+for language in "$@"
+do
+  echo "I can speak $language"
+done
---
+++ chapter1/2_11.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo $$
+
+echo $?
+
+sh 2_10.sh &
+
+echo $!
+
+echo $-
